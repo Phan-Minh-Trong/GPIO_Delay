d495780bf162e63e8a3fa1945cb197a58c92166f
Trong Phan <127284415+FanMinkJonk@users.noreply.github.com>
2025-10-20 10:56:23 +0700

    Finished Exercise 1

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -21,7 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#define ON_DELAY 2000
+#define OFF_DELAY 4000
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -94,6 +95,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, 1);
+    HAL_Delay(ON_DELAY);
+    HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, 0);
+    HAL_Delay(OFF_DELAY);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
